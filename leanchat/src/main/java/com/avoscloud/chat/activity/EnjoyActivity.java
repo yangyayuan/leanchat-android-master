f57e25f800b7d8073b574f7c6e25af36184d0b48
yangyayuan <741103702@qq.com>
2018-04-02 21:19:06 +0800

    framwork done

---
+++ leanchat/src/main/java/com/avoscloud/chat/activity/EnjoyActivity.java
@@ -0,0 +1,15 @@
+package com.avoscloud.chat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.avoscloud.chat.R;
+
+public class EnjoyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enjoy);
+    }
+}
--- leanchat/src/main/java/com/avoscloud/chat/activity/WebSiteActivity.java
+++ leanchat/src/main/java/com/avoscloud/chat/activity/WebSiteActivity.java
@@ -1,15 +1,35 @@
 package com.avoscloud.chat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.avoscloud.chat.R;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
 
 public class WebSiteActivity extends AppCompatActivity {
+
+    private MapView mMapview = null;
+    private BaiduMap mBaiduMap;
+    private ImageButton mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_site);
+        mMapview = (MapView)findViewById(R.id.mymapview);
+        mButton = (ImageButton) findViewById(R.id.mybutton2);
+        mBaiduMap = mMapview.getMap();
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WebSiteActivity.this,EnjoyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
